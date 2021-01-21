d45f0208428846ec5dd6991d1f85f89c26c5e39d
galeza <galeza.agnieszka@gmail.com>
2021-01-21 15:03:28 +0100

    Modify test

--- cypress/integration/tests/tests.spec.js
+++ cypress/integration/tests/tests.spec.js
@@ -8,7 +8,7 @@ describe("Test to check push", () => {
   
       it("Behance logo is visible and clickable", () => {
         
-        cy.get('button#onetrust-accept-btn-handler', { timeout: 20000 }).should('be.visible');
+       // cy.get('button#onetrust-accept-btn-handler', { timeout: 20000 }).should('be.visible');
         cy.get('div[class^="PrimaryNav-logoWrap"]').should("be.visible");
 
       });
